680cd3f7031a04ffd68210af86508bf593e8cf93
GcTzw <glamorouscoast@gmail.com>
2022-10-29 14:41:03 +0800

    Update 2022-10-29T06:40:56.807Z

---
+++ assets/js/all.js
@@ -0,0 +1,101 @@
+"use strict";
+
+var data = [{
+  "id": 0,
+  "name": "肥宅心碎賞櫻3日",
+  "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+  "area": "高雄",
+  "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+  "group": 87,
+  "price": 1400,
+  "rate": 10
+}, {
+  "id": 1,
+  "name": "貓空纜車雙程票",
+  "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "area": "台北",
+  "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+  "group": 99,
+  "price": 240,
+  "rate": 2
+}, {
+  "id": 2,
+  "name": "台中谷關溫泉會1日",
+  "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "area": "台中",
+  "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+  "group": 20,
+  "price": 1765,
+  "rate": 7
+}];
+init();
+
+function init() {
+  var tripArea = document.querySelector('.trip-area');
+  data.forEach(function (item) {
+    tripArea.innerHTML += getOneSet(item);
+  });
+}
+
+function getOneSet(obj) {
+  var rtnStr = "";
+  rtnStr += '<li class="trip-item">';
+  rtnStr += getImgArea(obj);
+  rtnStr += getInfoArea(obj);
+  rtnStr += '</li>';
+  return rtnStr;
+}
+
+function getImgArea(obj) {
+  var rtnStr = "";
+  rtnStr += '<div class="trip-item-img-area">';
+  rtnStr += "<img src=\"".concat(obj.imgUrl, "\" alt=\"\">");
+  rtnStr += "<div class=\"trip-item-location\">".concat(obj.area, "</div>");
+  rtnStr += "<div class=\"trip-item-rate\">".concat(obj.rate, "</div>");
+  rtnStr += '</div>';
+  return rtnStr;
+}
+
+function getInfoArea(obj) {
+  var rtnStr = "";
+  rtnStr += '<div class="trip-item-info-area">';
+  rtnStr += '<div>';
+  rtnStr += "<h4>".concat(obj.name, "</h4>");
+  rtnStr += "<p>".concat(obj.description, "</p>");
+  rtnStr += '</div>';
+  rtnStr += getInfoAreaFooter(obj);
+  rtnStr += '</div>';
+  return rtnStr;
+}
+
+function getInfoAreaFooter(obj) {
+  var rtnStr = "";
+  rtnStr += '<div class="d-flex justify-content-between">';
+  rtnStr += getInfoAreaRemain(obj);
+  rtnStr += getInfoAreaPrice(obj);
+  rtnStr += '</div>';
+  return rtnStr;
+}
+
+function getInfoAreaRemain(obj) {
+  var rtnStr = "";
+  rtnStr += '<div class="d-flex align-items-center">';
+  rtnStr += '<span class="material-symbols-outlined me-1">error</span>';
+  rtnStr += "\u5269\u4E0B\u6700\u5F8C ".concat(obj.group, " \u7D44");
+  rtnStr += '</div>';
+  return rtnStr;
+}
+
+function getInfoAreaPrice(obj) {
+  var rtnStr = "";
+  rtnStr += '<div class="d-flex align-items-center">';
+  rtnStr += '<span class="fs-6 me-1">TWD</span>';
+  rtnStr += "<span class=\"fs-2\">$".concat(toCurrency(obj.price), "</span>");
+  rtnStr += '</div>';
+  return rtnStr;
+}
+
+function toCurrency(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+//# sourceMappingURL=all.js.map
