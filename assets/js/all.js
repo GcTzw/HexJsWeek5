da268a3d6f0aebf0efa9f075471d99efe62296eb
GcTzw <glamorouscoast@gmail.com>
2022-11-06 13:10:53 +0800

    Update 2022-11-06T05:10:50.568Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -29,38 +29,51 @@ var objFieldNameArr = [{
   id: "txtDesc",
   name: "套票描述",
   objFieldName: "description"
-}];
-var data = [{
-  "id": 0,
-  "name": "肥宅心碎賞櫻3日",
-  "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-  "area": "高雄",
-  "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-  "group": 87,
-  "price": 1400,
-  "rate": 10
-}, {
-  "id": 1,
-  "name": "貓空纜車雙程票",
-  "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-  "area": "台北",
-  "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-  "group": 99,
-  "price": 240,
-  "rate": 2
-}, {
-  "id": 2,
-  "name": "台中谷關溫泉會1日",
-  "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-  "area": "台中",
-  "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-  "group": 20,
-  "price": 1765,
-  "rate": 7
-}];
-init("");
+}]; // let data = [
+//   {
+//     "id": 0,
+//     "name": "肥宅心碎賞櫻3日",
+//     "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+//     "area": "高雄",
+//     "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+//     "group": 87,
+//     "price": 1400,
+//     "rate": 10
+//   },
+//   {
+//     "id": 1,
+//     "name": "貓空纜車雙程票",
+//     "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//     "area": "台北",
+//     "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+//     "group": 99,
+//     "price": 240,
+//     "rate": 2
+//   },
+//   {
+//     "id": 2,
+//     "name": "台中谷關溫泉會1日",
+//     "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//     "area": "台中",
+//     "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+//     "group": 20,
+//     "price": 1765,
+//     "rate": 7
+//   }
+// ];
 
-function init(area) {
+var data = [];
+init();
+
+function init() {
+  axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json').then(function (response) {
+    console.log(response);
+    data = response.data.data;
+    renderData("");
+  });
+}
+
+function renderData(area) {
   var tripArea = document.querySelector('.trip-area');
   tripArea.innerHTML = "";
   var cnt = 0;
@@ -75,11 +88,12 @@ function init(area) {
   });
   var searchResult = document.querySelector('#searchResult');
   searchResult.innerText = cnt;
+  if (area == "") document.querySelector('#slcSearch').value = "所有";
 }
 
 function onchange_slcSearch() {
   var slcSearch = document.querySelector('#slcSearch');
-  init(slcSearch.value == "所有" ? "" : slcSearch.value);
+  renderData(slcSearch.value == "所有" ? "" : slcSearch.value);
 }
 
 function AddOneSet() {
@@ -144,7 +158,7 @@ function AddOneSet() {
       icon: 'success',
       confirmButtonColor: colorPrimary
     });
-    init("");
+    renderData("");
     clearAddPanel();
   }
 }
